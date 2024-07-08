6b714b8a8aa44bea75af5e6a382f58757a1316fd
majianguang <569445020@qq.com>
2024-07-08 15:06:19 +0800

    feat: 退出登录弹窗层级问题

--- web/app/components/header/account-dropdown-left/index.tsx
+++ web/app/components/header/account-dropdown-left/index.tsx
@@ -77,7 +77,7 @@ export default function AppSelector({ isMobile }: IAppSelecotr) {
               >
                 <Menu.Items
                   className="
-                    absolute left-full bottom-0 mt-1.5 w-60 max-w-80
+                    absolute left-full bottom-0 mt-1.5 w-60 max-w-80 z-10
                     divide-y divide-gray-100 origin-top-right rounded-lg bg-white
                     shadow-lg
                   "
