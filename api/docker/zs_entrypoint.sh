787d3d6b20795b614a380909d4163a5f81e209c6
liuzhaocheng <liuzhaocheng@zs-meta.com>
2024-07-18 20:14:38 +0800

    调整 docker-compose api 服务进程数，配置 zs_entrypoint.sh 启动脚本

---
+++ api/docker/zs_entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e -x
+
+echo "[api-debug] 启动调试模式"
+
+if [[ "${MIGRATION_ENABLED}" == "true" ]]; then
+  echo "Running migrations"
+  flask db upgrade
+fi
+
+cd /app/api
+
+gunicorn \
+  --bind "${DIFY_BIND_ADDRESS:-0.0.0.0}:${DIFY_PORT:-5001}" \
+  --workers ${SERVER_WORKER_AMOUNT:-1} \
+  --worker-class ${SERVER_WORKER_CLASS:-gevent} \
+  --timeout ${GUNICORN_TIMEOUT:-200} \
+  --preload \
+  app:app
