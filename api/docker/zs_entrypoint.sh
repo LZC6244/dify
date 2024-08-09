338ff78013e0b982784737bdb3b34cc2f4271c3b
liuzhaocheng <liuzhaocheng@zs-meta.com>
2024-08-09 10:30:43 +0800

    docker-compose-dev 运行配置优化

--- api/docker/zs_entrypoint.sh
+++ api/docker/zs_entrypoint.sh
@@ -20,13 +20,13 @@ ${CELERY_TASK_DEFAULT_QUEUE}:app_deletion\
 }
 
 if [[ "${MODE}" == "worker" ]]; then
-  celery -A app.celery worker -P ${CELERY_WORKER_CLASS:-gevent} -c ${CELERY_WORKER_AMOUNT:-1} --loglevel INFO \
+  celery -A app.celery worker -P ${CELERY_WORKER_CLASS:-prefork} -c ${CELERY_WORKER_AMOUNT:-1} --loglevel INFO \
     -Q ${DEFAULT_CELERY_QUEUES}
 elif [[ "${MODE}" == "api" ]]; then
   gunicorn \
     --bind "${DIFY_BIND_ADDRESS:-0.0.0.0}:${DIFY_PORT:-5001}" \
     --workers ${SERVER_WORKER_AMOUNT:-1} \
-    --worker-class ${SERVER_WORKER_CLASS:-gevent} \
+    --worker-class ${SERVER_WORKER_CLASS:-sync} \
     --timeout ${GUNICORN_TIMEOUT:-360} \
     --preload \
     app:app
