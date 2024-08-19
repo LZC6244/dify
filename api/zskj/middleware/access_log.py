882a06892b48ca28a8f8694c34871e6c7ef7927b
liuzhaocheng <liuzhaocheng@zs-meta.com>
2024-08-19 15:58:39 +0800

    为所有请求添加访问记录日志

---
+++ api/zskj/middleware/access_log.py
@@ -0,0 +1,31 @@
+
+import logging
+from flask import request
+from datetime import datetime
+
+
+logger = logging.getLogger(__name__)
+
+
+def access_start_log():
+    """
+    记录请求开始信息
+    :return:
+    """
+    request.zskj_start_time = datetime.now()
+    logger.info(
+        f"access_start: {request.method} - {request.path}")
+
+
+def access_end_log(response):
+    """
+    记录返回响应信息
+    :param response:
+    :return:
+    """
+    duration = datetime.now() - request.zskj_start_time
+    # 去除毫秒
+    # duration = str(duration).split('.')[0]
+    logger.info(
+        f"access_end: {response.status_code} - {request.path} - duration => {duration}")
+    return response
